c5743747307555e8ec1969e5ebbca766172f38c2
oren-BA <orenbenaltabe@gmail.com>
2020-11-28 22:37:08 +0200

    hi

--- matmul_functions.py
+++ matmul_functions.py
@@ -1,23 +1,50 @@
 import numpy as np
-from numba import njit, cuda
+from numba import njit, cuda, prange
 import timeit
 
 
 def matmul_transpose_trivial(X):
-    raise NotImplementedError("To be implemented")
+    C = np.zeros((X.shape[0], X.shape[0]))
+    for i in range(X.shape[0]):
+        for j in range(X.shape[0]):
+            sum = 0
+            for k in range(X.shape[1]):
+                sum += X[i][k]*X[k][j]
+            C[i][j] = sum
 
 
 @njit
 def matmul_transpose_numba(X):
-    raise NotImplementedError("To be implemented")
+    C = np.zeros((X.shape[0], X.shape[0]))
+    for i in prange(X.shape[0]):
+        for j in prange(X.shape[0]):
+            sum = 0
+            for k in prange(X.shape[1]):
+                sum += X[i][k] * X[k][j]
+            C[i][j] = sum
 
 
 def matmul_transpose_gpu(X):
-    raise NotImplementedError("To be implemented")
+    X_gpu = cuda.to_device(X)
+    C = np.zeros((X.shape[0], X.shape[0]))
+    matmul_kernel[1, 1024](X_gpu, C)
+    return C
 
 @cuda.jit
 def matmul_kernel(A, C):
-    raise NotImplementedError("To be implemented")
+    tx = cuda.threadIdx.x
+    for i in range(A.shape[0]):
+        for j in range(A.shape[0]):
+            s_arr = cuda.shared.array(1, dtype=np.int32)  # todo: I'm not sure regarding int64
+            # todo: maybe we should initialized to 0
+            iter_num = A.shape[1]//1024 if(A.shape[1] % 1024 ==0) else (A.shape[1]//1024 + 1)
+            for k in range(iter_num):
+                if 1024*k + tx < A.shape[1]:
+                  cuda.atomic.add(s_arr, 0, A[i][k*1024 +tx]*A[k*1024 +tx][j])
+            cuda.syncthreads()  # wait until all blocks finished
+
+            if tx == 0:
+                C[i][j] = s_arr[0]
 
 #this is the comparison function - keep it as it is, don't change X or Y.
 def matmul_comparison():
@@ -32,5 +59,5 @@ def matmul_comparison():
     print('CUDA:', timer(matmul_transpose_gpu, 1))
 
 
-if __name__ == '__main__':
+if __name__ == '_main_':
     matmul_comparison()
