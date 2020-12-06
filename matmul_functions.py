621f5a81e9ac63d6ceffc697d61390aa5d670818
liavzafar <62476836+liavzafar@users.noreply.github.com>
2020-12-06 03:58:14 +0200

    2 seconds for part 3 and report.docx

--- matmul_functions.py
+++ matmul_functions.py
@@ -9,8 +9,9 @@ def matmul_transpose_trivial(X):
         for j in range(X.shape[0]):
             sum = 0
             for k in range(X.shape[1]):
-                sum += X[i][k]*X[k][j]
+                sum += X[i][k]*X[j][k]
             C[i][j] = sum
+    return C
 
 
 @njit
@@ -20,9 +21,9 @@ def matmul_transpose_numba(X):
         for j in prange(X.shape[0]):
             sum = 0
             for k in prange(X.shape[1]):
-                sum += X[i][k] * X[k][j]
+                sum += X[i][k] *X[j][k]
             C[i][j] = sum
-
+    return C
 
 def matmul_transpose_gpu(X):
     X_gpu = cuda.to_device(X)
@@ -32,16 +33,18 @@ def matmul_transpose_gpu(X):
 
 @cuda.jit
 def matmul_kernel(A, C):
-    # n, m = C.shape
-    # tx = cuda.threadIdx.x
-    # i=0
-    # while 1024*i < n**2:
-    #     if (1024*i + tx) <n**2:
-    #         sum = 0
-    #         for k in range(m):
-    #             sum += A[(1024*i+tx)//n][k]*A[k][(1024*i +tx) % n]
-    #         C[(1024*i+tx)//n][(1024*i +tx) % n] = sum
-    #     i += 1
+    n, m = A.shape
+    tx = cuda.threadIdx.x
+    i = 0
+    while 1024*i < n**2:
+        if (1024*i + tx) < n**2:
+            i_index = (1024*i+tx)//n
+            j_index = (1024*i+tx) % n
+            sum = 0
+            for k in range(m):
+                sum += A[i_index][k]*A[j_index][k]
+            C[i_index][j_index] = sum
+        i += 1
 
 
     # this works really well with m>=1000 (actually if this 1001,2001 it will not be that good..
@@ -59,20 +62,38 @@ def matmul_kernel(A, C):
     #            C[i][j] = s_arr[0]
 
     # ***********************
-    tx = int(cuda.threadIdx.x)
-    thread_rows = A.shape[0] // 1024
-    first_row = 0
-    if tx < A.shape[0] % 1024:
-        thread_rows += 1
-        first_row = tx*thread_rows
-    if tx > A.shape[0] % 1024:
-        first_row = thread_rows*tx + A.shape[0] % 1024
-    for i in range(first_row, first_row + thread_rows):
-        for j in range(A.shape[0]):
-            sum = 0
-            for k in range(A.shape[1]):
-                sum += A[i][k]*A[k][j]
-            C[i][j] = sum
+    #tx = int(cuda.threadIdx.x)
+    #thread_rows = A.shape[0] // 1024
+    #first_row = 0
+    #if tx < A.shape[0] % 1024:
+    #    thread_rows += 1
+    #    first_row = tx*thread_rows
+    #if tx > A.shape[0] % 1024:
+    #    first_row = thread_rows*tx + A.shape[0] % 1024
+    #for i in range(first_row, first_row + thread_rows):
+    #    for j in range(A.shape[0]):
+    #        sum = 0
+    #        for k in range(A.shape[1]):
+    #           sum += A[i][k]*A[k][j]
+    #      C[i][j] = sum
+
+    #n, m = A.shape
+    #stide = (n**2)//1024
+    #tx = cuda.threadIdx.x
+    #i = tx*stide
+    #while i < (tx+1)*stide:
+    #    sum = 0
+    #    for k in range(m):
+    #        sum += A[i//n][k]*A[k][i % n]
+    #    C[i//n][i % n] = sum
+    #    i += 1
+    #if tx < stide:  # some threads need to work a bit harder :)
+    #    sum = 0
+    #    for k in range(m):
+    #        sum += A[(n**2 - tx)// n][k] * A[k][(n**2 - tx) % n]
+    #    C[(n**2 - tx) // n][(n**2 - tx) % n] = sum
+
+
 
 #this is the comparison function - keep it as it is, don't change X or Y.
 def matmul_comparison():
